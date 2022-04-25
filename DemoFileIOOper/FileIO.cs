9bc25361e3f50fc208c2327abb9b47880a5a5f4a
Harry <hareshwarp649>
2022-04-25 12:43:43 +0530

    [HRPatil]Addwd: File I/O Operation demo and Serialization and Deserialization  process

---
+++ DemoFileIOOper/FileIO.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace DemoFileIOOper
+{
+    [Serializable]
+    public class Demo
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+       
+    }
+        public class FileIO
+        {
+            //File Path
+            const string FilePath = @"C:\Users\ADMIN\source\repos\FileIOperation\DemoFileIOOpeation\DemoFileIOOper\file1.txt";
+            const string FilePath_Serializeddata = @"C:\Users\ADMIN\source\repos\FileIOperation\DemoFileIOOpeation\DemoFileIOOper\File2.txt";
+            const string FilePath_XmlSerializeddata = @"C:\Users\ADMIN\source\repos\FileIOperation\DemoFileIOOpeation\DemoFileIOOper\File3.txt";
+            /// <summary>
+            /// Serialization-object to binary
+            /// </summary>
+            public static void BinarySerialization()
+            {
+                List<Demo> data = new List<Demo>()
+            {
+                new Demo {Age = 25, Name = "Deva" },
+                new Demo {Age = 24, Name = "Shree" },
+                new Demo {Age = 23, Name = "Vande" },
+            };
+                FileStream streamdata = new FileStream(FilePath_Serializeddata, FileMode.Create);
+                BinaryFormatter bn = new BinaryFormatter();
+                bn.Serialize(streamdata, data);
+                streamdata.Close();
+                streamdata.Dispose();
+                Console.WriteLine("*** Convert Object To Binary ***");
+                string binaryTxt = File.ReadAllText(FilePath_Serializeddata);
+                Console.WriteLine(binaryTxt);
+                Console.WriteLine("\n=====================================================\n");
+            }
+           
+            public static void BinaryDeserialization()
+            {
+                FileStream streamdata = new FileStream(FilePath_Serializeddata, FileMode.Open);
+                BinaryFormatter bn = new BinaryFormatter();
+                List<Demo> data = (List<Demo>)bn.Deserialize(streamdata);
+                streamdata.Close();
+                Console.WriteLine("*** Convert List Of Binary Data To Object***");
+                foreach (var contact in data)
+                {
+                    Console.WriteLine(contact.Name);
+                    Console.WriteLine(contact.Age);
+                }
+                Console.WriteLine("\n=====================================================\n");
+            }
+
+        internal static void XmlSerialization()
+        {
+            throw new NotImplementedException();
+        }
+
+      
+        public static void ReadData()
+            {
+                if (File.Exists(FilePath))
+                {
+                    var data = File.ReadAllText(FilePath);
+                    Console.WriteLine(data);
+                    using (StreamReader sr = new StreamReader(FilePath))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            Console.WriteLine(sr.ReadLine());
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File is not exist");
+                }
+            }
+           
+            public static void ReadDataUsingStreamReader()
+            {
+                if (File.Exists(FilePath))
+                {
+                    // Create an instance of StreamReader to read from a file.
+                    // The using statement also closes the StreamReader
+                    using (StreamReader sr = new StreamReader(FilePath))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            Console.WriteLine(sr.ReadLine());
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File is not exist");
+                }
+            }
+            /// <summary>
+            /// Overwrite the existing data
+            /// </summary>
+            /// <param name="newline"></param>
+            public static void OverWriteData(string newline = "")
+            {
+                File.WriteAllText(FilePath, newline);
+                using (StreamReader sr = new StreamReader(FilePath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        Console.WriteLine(sr.ReadLine());
+                    }
+                }
+            }
+           
+            public static void AppendData(string newline = "", bool append = true)
+            {
+                File.AppendAllText(FilePath, newline);
+                using (StreamReader sr = new StreamReader(FilePath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        Console.WriteLine(sr.ReadLine());
+                    }
+                }
+            }
+        }
+}
---
+++ DemoFileIOOper/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoFileIOOper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool status = true;
+
+            while (status)
+            {
+                Console.WriteLine("Select\n1)Check File is exits or not And read data\n2)Using StreamReader\n3)OverWriteData\n" +
+                    "4)AppendData\n5)Serialization and Deserialization using binary format\n6)Serialization and Deserialization using XML format");
+                int op = Convert.ToInt32(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        FileIO.ReadData();
+                        break;
+                    case 2:
+                        FileIO.ReadDataUsingStreamReader();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter Data");
+                        string data = Console.ReadLine();
+                        Console.WriteLine($"Added - {data} ");
+                        FileIO.OverWriteData(data);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter Data");
+                        string data1 = Console.ReadLine();
+                        Console.WriteLine($"Added - {data1} ");
+                        FileIO.AppendData(data1, true);
+                        break;
+                    case 5:
+                        Console.WriteLine("1)Serialization\n2)Deserialization");
+                        int op2 = Convert.ToInt32(Console.ReadLine());
+                        switch (op2)
+                        {
+                            case 1:
+                                FileIO.BinarySerialization();
+                                Console.WriteLine("Your Data is serialized in binary format");
+                                break;
+                            case 2:
+                                FileIO.BinaryDeserialization();
+                                Console.WriteLine("Your Data is Deserialized");
+                                break;
+                            default:
+                                Console.WriteLine("invalid option");
+                                break;
+                        }
+                        break;
+                    case 6:
+                        Console.WriteLine("1)Serialization\n2)Deserialization");
+                        int op3 = Convert.ToInt32(Console.ReadLine());
+                        switch (op3)
+                        {
+                            case 1:
+                                FileIO.XmlSerialization();
+                                Console.WriteLine("Your Data is serialized in XML format");
+                                break;
+                            case 2:
+                                FileIO.XmlSerialization();
+                                Console.WriteLine("Your Data is Deserialized");
+                                break;
+                            default:
+                                Console.WriteLine("invalid option");
+                                break;
+                        }
+                        break;
+                    default:
+                        status = !status;
+                        break;
+                }
+            }
+        }
+    }
+}
